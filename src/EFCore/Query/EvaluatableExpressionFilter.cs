724cb658c02a3e36f2a77111722d80331823bb92
Shay Rojansky <roji@roji.org>
2019-07-26 18:58:47 +0200

    Make EvaluableExpressionFilter public

--- src/EFCore/Query/EvaluatableExpressionFilter.cs
+++ src/EFCore/Query/EvaluatableExpressionFilter.cs
@@ -1,12 +1,13 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
 using System.Linq.Expressions;
 using System.Reflection;
+using Microsoft.EntityFrameworkCore.Query.Internal;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Microsoft.EntityFrameworkCore.Query.Internal
+namespace Microsoft.EntityFrameworkCore.Query
 {
     /// <summary>
     ///     <para>
